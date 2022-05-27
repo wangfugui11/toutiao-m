114fa80b5bf075d825c39994a8326c18724f735a
wangfugui <1779162994@qq.com>
2022-05-27 15:31:54 +0800

    ‘提交初始化的目录结构’

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入入全局样式(没补全index.js没法子热更新)
+import './style/index.css'
+
 Vue.config.productionTip = false
 
 new Vue({
